58f7e5f3abdb105e706a2f8ee77669a246a72a0b
satendra <45224591+satendra17@users.noreply.github.com>
2022-09-23 18:32:04 +0530

    Time: 7 ms (57.32%), Space: 11.2 MB (29.23%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int bs(vector<int>nums,int l,int h,int target){
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]>target)h=mid-1;
-            else l=mid+1;
+        int bs(vector<int>&nums,int target,int l,int h){
+                while(l<=h){
+                        int mid=(l+h)/2;
+                        if(nums[mid]==target)return mid;
+                        else if(nums[mid]<target)l=mid+1;
+                        else h=mid-1;
+                }
+                return -1;
         }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1,ans=0;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(mid<h&&nums[mid]>nums[mid+1]){ans=mid; break;}
-            if(mid>l&&nums[mid-1]>nums[mid]){ans=mid-1;break;}
-            
-            if(nums[l]>nums[mid])h=mid-1;
-            else l=mid+1;
-            
-        }
-        
-        if(nums[ans]==target)return ans;
-        int left= bs(nums,0,ans-1,target);
-        if(left!=-1) return left;
-        return bs(nums,ans+1,nums.size()-1,target);
-            
-        
+            int l=0,h=nums.size()-1;
+            while(l<h){
+                    int mid=l+(h-l)/2;
+                    cout<<mid;
+                    if(nums[mid]==target)return mid;
+                   
+                    else if(nums[mid]>=nums[l]){
+                            if(target>=nums[l] and target<=nums[mid])return bs(nums,target,l,mid-1);
+                            else l=mid+1;
+                    }
+                    else if(nums[mid]<=nums[h]){
+                            if(target>=nums[mid] and target<=nums[h])return bs(nums,target,mid+1,h);
+                            else h=mid-1;
+                            
+                    }
+            }
+           if(nums[l]==target or nums[h]==target)return l;
+       //cout<<nums[h]; 
+        return -1;
     }
 };
