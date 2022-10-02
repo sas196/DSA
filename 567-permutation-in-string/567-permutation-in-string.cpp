2a113477d975d0d643d9cd5ef6f26ba32748ada1
satendra <45224591+satendra17@users.noreply.github.com>
2022-10-02 17:58:40 +0530

    Time: 20 ms (46.16%), Space: 7.3 MB (45.06%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -4,17 +4,17 @@ public:
         if(s1.size()>s2.size())return false;
         vector<int>hash1(26,0);
         vector<int>hash2(26,0);
-        for(int i=0;i<s1.size();i++){
-            hash1[s1[i]-'a']++;
-            hash2[s2[i]-'a']++;
-        }
-        if(hash1==hash2)return true;
-        int ci=1,end=s1.size();
-        while(end<s2.size()){
-            hash2[s2[ci-1]-'a']--;
-            hash2[s2[end]-'a']++;
-            if(hash2==hash1)return true;
-            ci++;end++;
+            for(int i=0;i<s1.size();i++){
+                    hash1[s1[i]-'a']++;
+                   hash2[s2[i]-'a']++;
+            }
+        int i=1,j=s1.size();
+            if(hash1==hash2)return true;
+        while(j<s2.size()){
+                hash2[s2[j]-'a']++;
+                hash2[s2[i-1]-'a']--;
+                if(hash1==hash2)return true;
+                i++;j++;
         }
         return false;
     }
